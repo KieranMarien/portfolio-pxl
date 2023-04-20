afb7e5cd8c13b619e3a062aa21f02b3b0f4be374
KieranMarienPxl <marieki@cronos.be>
2023-04-20 15:58:23 +0200

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,5 @@ import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <> <Component {...pageProps} /><Analytics /> <title>Kieran Marien Portfolio page</title>
-  <meta name="description" content="My PXL portfolio website. Contains my activities with some extra information." /></>
+  return <> <Component {...pageProps} /><Analytics /> </>
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,16 @@ import { Inter } from 'next/font/google'
 import Intro from '@/components/intro'
 import Navbar from '@/components/navbar'
 import Link from 'next/link'
-
+import Head from 'next/head'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     
 <main className="bg-pattern flex min-h-screen flex-col items-center justify-between p-24">
+<title>Kieran Marien Portfolio page</title>
+<Head>
+<meta name="description" content="My PXL portfolio website. Contains my activities with some extra information." /></Head>
     <div className="z-10 w-full max-w-5xl flex flex-col items-center justify-between font-mono text-sm lg:flex-row ">
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
             By  { 'Kieran Marien'}
